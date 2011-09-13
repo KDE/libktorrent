32082ce759d4578dc7ee77156faa75a4446ae0e5
Joris Guisson <joris.guisson@gmail.com>
2011-09-13 19:21:50 +0200

    Start of network layer code refactoring

---
+++ src/net/trafficshapedsocket.cpp
@@ -0,0 +1,148 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Joris Guisson                                   *
+ *   joris.guisson@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+
+
+#include "trafficshapedsocket.h"
+#include "speed.h"
+#include "socket.h"
+
+#define OUTPUT_BUFFER_SIZE 16393
+
+using namespace bt;
+
+namespace net
+{
+
+	TrafficShapedSocket::TrafficShapedSocket(SocketDevice* sock) :
+		rdr(0),
+		up_gid(0),
+		down_gid(0),
+		sock(sock)
+	{
+		down_speed = new Speed();
+		up_speed = new Speed();
+	}
+
+	TrafficShapedSocket::TrafficShapedSocket(int fd, int ip_version) :
+		rdr(0),
+		up_gid(0),
+		down_gid(0)
+	{
+		sock = new Socket(fd, ip_version);
+		down_speed = new Speed();
+		up_speed = new Speed();
+	}
+
+	TrafficShapedSocket::TrafficShapedSocket(bool tcp, int ip_version) :
+		rdr(0),
+		up_gid(0),
+		down_gid(0)
+	{
+		sock = new Socket(tcp, ip_version);
+		down_speed = new Speed();
+		up_speed = new Speed();
+	}
+	
+	TrafficShapedSocket::~TrafficShapedSocket()
+	{
+		delete up_speed;
+		delete down_speed;
+		delete sock;
+	}
+	
+	void TrafficShapedSocket::setGroupID(Uint32 gid,bool upload)
+	{
+		if (upload)
+			up_gid = gid;
+		else
+			down_gid = gid;
+	}
+	
+	float TrafficShapedSocket::getDownloadRate() const
+	{
+		mutex.lock();
+		float ret = down_speed->getRate();
+		mutex.unlock();
+		return ret;
+	}
+	
+	float TrafficShapedSocket::getUploadRate() const
+	{
+		mutex.lock();
+		float ret = up_speed->getRate();
+		mutex.unlock();
+		return ret;
+	}
+	
+	void TrafficShapedSocket::updateSpeeds(bt::TimeStamp now)
+	{
+		mutex.lock();
+		up_speed->update(now);
+		down_speed->update(now);
+		mutex.unlock();
+	}
+	
+	static bt::Uint8 input_buffer[OUTPUT_BUFFER_SIZE];
+	
+	Uint32 TrafficShapedSocket::read(bt::Uint32 max_bytes_to_read, bt::TimeStamp now)
+	{
+		Uint32 br = 0;
+		bool no_limit = (max_bytes_to_read == 0);
+		Uint32 ba = sock->bytesAvailable();
+		if (ba == 0)
+		{
+			// For some strange reason, sometimes bytesAvailable returns 0, while there are 
+			// bytes to read, so give ba the maximum value it can be
+			ba = max_bytes_to_read > 0 ? max_bytes_to_read : OUTPUT_BUFFER_SIZE;
+		}
+		
+		while ((br < max_bytes_to_read || no_limit)  && ba > 0)
+		{
+			Uint32 tr = ba;
+			if (tr > OUTPUT_BUFFER_SIZE)
+				tr = OUTPUT_BUFFER_SIZE;
+			if (!no_limit && tr + br > max_bytes_to_read)
+				tr = max_bytes_to_read - br;
+			
+			int ret = sock->recv(input_buffer,tr);
+			if (ret > 0)
+			{
+				mutex.lock();
+				down_speed->onData(ret,now);
+				mutex.unlock();
+				if (rdr)
+					rdr->onDataReady(input_buffer,ret);
+				br += ret;
+				ba -= ret;
+			}
+			else if (ret < 0)
+			{
+				return br;
+			}
+			else
+			{
+				sock->close();
+				return br;
+			}
+		}
+		return br;
+	}
+
+}
---
+++ src/net/trafficshapedsocket.h
@@ -0,0 +1,121 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Joris Guisson                                   *
+ *   joris.guisson@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+
+
+#ifndef NET_TRAFFICSHAPEDSOCKET_H
+#define NET_TRAFFICSHAPEDSOCKET_H
+
+#include <QMutex>
+#include <net/socketdevice.h>
+#include <util/constants.h>
+
+namespace net 
+{
+	class Speed;
+	
+	class SocketReader
+	{
+	public:
+		SocketReader() {}
+		virtual ~SocketReader() {}
+		
+		/**
+		 * Function which will be called whenever data has been read from the socket.
+		 * This data should be dealt with, otherwise it will be discarded.
+		 * @param buf The buffer
+		 * @param size The size of the buffer
+		 */
+		virtual void onDataReady(bt::Uint8* buf, bt::Uint32 size) = 0;	
+	};
+
+	/**
+	 * Socket which supports traffic shaping
+	 */
+	class  TrafficShapedSocket
+	{
+	public:
+		TrafficShapedSocket(SocketDevice* sock);
+		TrafficShapedSocket(int fd,int ip_version);
+		TrafficShapedSocket(bool tcp,int ip_version);
+		virtual ~TrafficShapedSocket();
+		
+		/// Get the SocketDevice
+		SocketDevice* socketDevice() {return sock;}
+		
+		/// Get the SocketDevice (const vesion)
+		const SocketDevice* socketDevice() const {return sock;}
+		
+		/// Set the reader
+		void setReader(SocketReader* r) {rdr = r;}
+		
+		/**
+		 * Reads data from the socket and pass it to the SocketReader.
+		 * @param max_bytes_to_read Maximum number of bytes to read (0 is no limit)
+		 * @param now Current time stamp
+		 * @return The number of bytes read
+		 */
+		virtual Uint32 read(Uint32 max_bytes_to_read, bt::TimeStamp now);
+		
+		/**
+		 * Writes data to the socket. Subclasses should implement the data source.
+		 * @param max The maximum number of bytes to send over the socket (0 = no limit)
+		 * @param now Current time stamp
+		 * @return The number of bytes written
+		 */
+		virtual Uint32 write(Uint32 max, bt::TimeStamp now) = 0;
+		
+		/// See if the socket has something ready to write
+		virtual bool bytesReadyToWrite() const = 0;
+		
+		/// Get the current download rate
+		float getDownloadRate() const;
+		
+		/// Get the current download rate
+		float getUploadRate() const;
+		
+		/// Update up and down speed
+		void updateSpeeds(bt::TimeStamp now);
+		
+		/**
+		 * Set the group ID of the socket
+		 * @param gid THe ID (0 is default group)
+		 * @param upload Whether this is an upload group or a download group
+		 */
+		void setGroupID(Uint32 gid,bool upload);
+		
+		/// Get the download group ID
+		Uint32 downloadGroupID() const {return down_gid;}
+		
+		/// Get the upload group ID
+		Uint32 uploadGroupID() const {return up_gid;}
+		
+	protected:
+		SocketReader* rdr;
+		Speed* down_speed;
+		Speed* up_speed;
+		Uint32 up_gid;
+		Uint32 down_gid; // group id which this torrent belongs to, group 0 means the default group
+		SocketDevice* sock;
+		mutable QMutex mutex;
+	};
+
+}
+
+#endif // NET_TRAFFICSHAPEDSOCKET_H
