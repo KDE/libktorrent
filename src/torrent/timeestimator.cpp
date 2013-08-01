ce7f61416b3c7c26033511ced236381b04da85a2
Joris Guisson <joris.guisson@gmail.com>
2013-08-01 11:15:32 +0200

    Fix bug causing eta estimation to go wrong during uploading
    BUG: 320699

--- src/torrent/timeestimator.cpp
+++ src/torrent/timeestimator.cpp
@@ -27,285 +27,250 @@
 
 namespace bt
 {
-	TimeEstimator::ETAlgorithm TimeEstimator::m_algorithm = ETA_KT;
 
-	TimeEstimator::TimeEstimator(TorrentControl* tc)
-		: m_tc(tc)
-	{
-		m_samples = new SampleQueue(20);
-		m_lastAvg = 0;
-		m_perc = -1;
-	}
-
-
-	TimeEstimator::~TimeEstimator()
-	{
-		delete m_samples;
-	}
-
-	Uint32 TimeEstimator::sample() const
-	{
-		const TorrentStats& s = m_tc->getStats();
-		if(s.completed)
-		{
-			return s.upload_rate;
-		}
-		else
-		{
-			return s.download_rate;
-		}
-	}
-	Uint64 TimeEstimator::bytesLeft() const
-	{
-		const TorrentStats& s = m_tc->getStats();
-		if(s.completed)
-		{
-			if(s.max_share_ratio >= 0.01f)
-			{
-				float ratio = s.shareRatio();
-				float delta = s.max_share_ratio - ratio;
-				if(delta <= 0.0f)
-					return -1;
-				else
-					return s.bytes_downloaded * delta - s.bytes_uploaded;
-			}
-			else
-				return -1;
-		}
-		else
-		{
-			return s.bytes_left_to_download;
-		}
-	}
-
-	int TimeEstimator::estimate()
-	{
-		const TorrentStats& s = m_tc->getStats();
+    TimeEstimator::TimeEstimator(TorrentControl* tc)
+        : m_tc(tc)
+    {
+        m_samples = new SampleQueue(20);
+        m_lastAvg = 0;
+        m_perc = -1;
+    }
 
 
-		// in seeding mode check if we still need to seed
-		if(s.completed)
-		{
-			if(bytesLeft() == 0 || s.max_share_ratio < 0.01f)
-				return ALREADY_FINISHED;
-		}
-		
-		// only estimate when we are running
-		if(!s.running || s.paused)
-		{	
-			return NEVER;
-		}
+    TimeEstimator::~TimeEstimator()
+    {
+        delete m_samples;
+    }
+
+    Uint32 TimeEstimator::sample() const
+    {
+        const TorrentStats& s = m_tc->getStats();
+        if(s.completed)
+        {
+            return s.upload_rate;
+        }
+        else
+        {
+            return s.download_rate;
+        }
+    }
+
+    Uint64 TimeEstimator::bytesLeft() const
+    {
+        const TorrentStats& s = m_tc->getStats();
+        if(s.completed)
+        {
+            if(s.max_share_ratio >= 0.01f)
+            {
+                float ratio = s.shareRatio();
+                float delta = s.max_share_ratio - ratio;
+                if(delta <= 0.0f)
+                    return 0;
+
+                if(s.bytes_downloaded * delta < s.bytes_downloaded)
+                    return 0;
+                else
+                    return s.bytes_downloaded * delta - s.bytes_uploaded;
+            }
+            else
+                return 0;
+        }
+        else
+        {
+            return s.bytes_left_to_download;
+        }
+    }
+
+    int TimeEstimator::estimate()
+    {
+        const TorrentStats& s = m_tc->getStats();
 
 
-		//ones without pre-calculations
-		switch(m_algorithm)
-		{
+        // in seeding mode check if we still need to seed
+        if(s.completed)
+        {
+            if(bytesLeft() == 0 || s.max_share_ratio < 0.01f)
+                return ALREADY_FINISHED;
+        }
 
-		case ETA_CSA:
-			return estimateCSA();
+        // only estimate when we are running
+        if(!s.running || s.paused)
+        {
+            return NEVER;
+        }
 
-		case ETA_GASA:
-			return estimateGASA();
+        return estimateKT();
+    }
 
-		case ETA_KT:
-			return estimateKT();
+    int TimeEstimator::estimateGASA()
+    {
+        const TorrentStats& s = m_tc->getStats();
 
-		case ETA_MAVG:
-			m_samples->push(sample());
-			return estimateMAVG();
+        if(m_tc->getRunningTimeDL() > 0 && s.bytes_downloaded > 0)
+        {
+            Uint64 d = s.bytes_downloaded;
+            if(s.imported_bytes <= s.bytes_downloaded)
+                d -= s.imported_bytes;
+            double avg_speed = (double) d / (double) m_tc->getRunningTimeDL();
+            return (Uint32) floor((double) bytesLeft() / avg_speed);
+        }
 
-		case ETA_WINX:
-			m_samples->push(sample());
-			return estimateWINX();
-		default:
-			return NEVER;
-		}
-	}
+        return NEVER;
+    }
 
-	int TimeEstimator::estimateCSA()
-	{
-		const TorrentStats& s = m_tc->getStats();
+    int TimeEstimator::estimateWINX()
+    {
+        if(m_samples->sum() > 0 && m_samples->count() > 0)
+            return (Uint32) floor((double) bytesLeft() / ((double) m_samples->sum() / (double) m_samples->count()));
 
-		if(s.download_rate == 0)
-			return NEVER;
+        return NEVER;
+    }
 
-		return (int)floor((float)bytesLeft() / (float)s.download_rate);
-	}
+    int TimeEstimator::estimateMAVG()
+    {
+        if(m_samples->count() > 0)
+        {
+            double lavg;
 
-	int TimeEstimator::estimateGASA()
-	{
-		const TorrentStats& s = m_tc->getStats();
+            if(m_lastAvg == 0)
+                lavg = (Uint32) m_samples->sum() / m_samples->count();
+            else
+                lavg = m_lastAvg - ((double) m_samples->first() / (double) m_samples->count()) + ((double) m_samples->last() / (double) m_samples->count());
 
-		if(m_tc->getRunningTimeDL() > 0 && s.bytes_downloaded > 0)
-		{
-			Uint64 d = s.bytes_downloaded;
-			if(s.imported_bytes <= s.bytes_downloaded)
-				d -= s.imported_bytes;
-			double avg_speed = (double) d / (double) m_tc->getRunningTimeDL();
-			return (Uint32) floor((double) bytesLeft() / avg_speed);
-		}
+            m_lastAvg = (Uint32) floor(lavg);
 
-		return NEVER;
-	}
+            if(lavg > 0)
+                return (Uint32) floor((double) bytesLeft() / ((lavg + (m_samples->sum() / m_samples->count())) / 2));
 
-	int TimeEstimator::estimateWINX()
-	{
-		if(m_samples->sum() > 0 && m_samples->count() > 0)
-			return (Uint32) floor((double) bytesLeft() / ((double) m_samples->sum() / (double) m_samples->count()));
+            return NEVER;
+        }
 
-		return NEVER;
-	}
+        return NEVER;
+    }
 
-	int TimeEstimator::estimateMAVG()
-	{
-		if(m_samples->count() > 0)
-		{
-			double lavg;
+    SampleQueue::SampleQueue(int max)
+        : m_size(max), m_count(0)
+    {
+        m_samples = new Uint32[max];
 
-			if(m_lastAvg == 0)
-				lavg = (Uint32) m_samples->sum() / m_samples->count();
-			else
-				lavg = m_lastAvg - ((double) m_samples->first() / (double) m_samples->count()) + ((double) m_samples->last() / (double) m_samples->count());
+        for(int i = 0; i < m_size; ++i)
+            m_samples[i] = 0;
 
-			m_lastAvg = (Uint32) floor(lavg);
+        m_end = -1;
 
-			if(lavg > 0)
-				return (Uint32) floor((double) bytesLeft() / ((lavg + (m_samples->sum() / m_samples->count())) / 2));
+        m_start = 0;
+    }
 
-			return NEVER;
-		}
+    SampleQueue::~ SampleQueue()
+    {
+        delete [] m_samples;
+    }
 
-		return NEVER;
-	}
+    void SampleQueue::push(Uint32 sample)
+    {
+        if(m_count < m_size)
+        {
+            //it's not full yet
+            m_samples[(++m_end) % m_size ] = sample;
+            m_count++;
 
-	SampleQueue::SampleQueue(int max)
-		: m_size(max), m_count(0)
-	{
-		m_samples = new Uint32[max];
+            return;
+        }
 
-		for(int i = 0; i < m_size; ++i)
-			m_samples[i] = 0;
+        //since it's full I'll just replace the oldest value with new one and update all variables.
+        m_end = (m_end + 1) % m_size;
+        m_start = (m_start + 1) % m_size;
+        m_samples[m_end] = sample;
+    }
 
-		m_end = -1;
+    Uint32 SampleQueue::first()
+    {
+        return m_samples[m_start];
+    }
 
-		m_start = 0;
-	}
+    Uint32 SampleQueue::last()
+    {
+        return m_samples[m_end];
+    }
 
-	SampleQueue::~ SampleQueue()
-	{
-		delete [] m_samples;
-	}
+    bool SampleQueue::isFull()
+    {
+        return m_count >= m_size;
+    }
 
-	void SampleQueue::push(Uint32 sample)
-	{
-		if(m_count < m_size)
-		{
-			//it's not full yet
-			m_samples[(++m_end) % m_size ] = sample;
-			m_count++;
+    int SampleQueue::count()
+    {
+        return m_count;
+    }
 
-			return;
-		}
+    Uint32 SampleQueue::sum()
+    {
+        Uint32 s = 0;
 
-		//since it's full I'll just replace the oldest value with new one and update all variables.
-		m_end = (m_end + 1) % m_size;
-		m_start = (m_start + 1) % m_size;
-		m_samples[m_end] = sample;
-	}
+        for(int i = 0; i < m_count; ++i)
+            s += m_samples[i];
 
-	Uint32 SampleQueue::first()
-	{
-		return m_samples[m_start];
-	}
+        return s;
+    }
 
-	Uint32 SampleQueue::last()
-	{
-		return m_samples[m_end];
-	}
-
-	bool SampleQueue::isFull()
-	{
-		return m_count >= m_size;
-	}
-
-	int SampleQueue::count()
-	{
-		return m_count;
-	}
-
-	Uint32 SampleQueue::sum()
-	{
-		Uint32 s = 0;
-
-		for(int i = 0; i < m_count; ++i)
-			s += m_samples[i];
-
-		return s;
-	}
-
-	void TimeEstimator::setAlgorithm(ETAlgorithm theValue)
-	{
-		m_algorithm = theValue;
-	}
-
-	int TimeEstimator::estimateKT()
-	{
-		const TorrentStats& s = m_tc->getStats();
+    int TimeEstimator::estimateKT()
+    {
+        const TorrentStats& s = m_tc->getStats();
 
 
-		//push new sample
-		m_samples->push(sample());
+        //push new sample
+        m_samples->push(sample());
 
-		if(s.completed)
-			return estimateWINX();
+        if(s.completed)
+            return estimateWINX();
 
-		double perc = (double) s.bytes_downloaded / (double) s.total_bytes;
+        double perc = (double) s.bytes_downloaded / (double) s.total_bytes;
 
-		int percentage = (int)(perc) * 100;
+        int percentage = (int)(perc) * 100;
 
-		//calculate percentage increasement
-		double delta = 1 - 1 / (perc / m_perc);
+        //calculate percentage increasement
+        double delta = 1 - 1 / (perc / m_perc);
 
-		//remember last percentage
-		m_perc = perc;
+        //remember last percentage
+        m_perc = perc;
 
 
-		if(s.bytes_downloaded < 1024 * 1024 * 100 && m_samples->last() > 0) // < 100KB
-		{
-			m_lastETA = estimateGASA();
-			return m_lastETA;
-		}
+        if(s.bytes_downloaded < 1024 * 1024 * 100 && m_samples->last() > 0) // < 100KB
+        {
+            m_lastETA = estimateGASA();
+            return m_lastETA;
+        }
 
-		if(percentage >= 99 && m_samples->last() > 0 && bytesLeft() <= 10 * 1024 * 1024 * 1024LL) //1% of a very large torrent could be hundreds of MB so limit it to 10MB
-		{
+        if(percentage >= 99 && m_samples->last() > 0 && bytesLeft() <= 10 * 1024 * 1024 * 1024LL) //1% of a very large torrent could be hundreds of MB so limit it to 10MB
+        {
 
-			if(!m_samples->isFull())
-			{
-				m_lastETA = estimateWINX();
+            if(!m_samples->isFull())
+            {
+                m_lastETA = estimateWINX();
 
-				if(m_lastETA == 0)
-					m_lastETA = estimateGASA();
+                if(m_lastETA == 0)
+                    m_lastETA = estimateGASA();
 
-				return m_lastETA;
-			}
-			else
-			{
-				m_lastETA = 0;
+                return m_lastETA;
+            }
+            else
+            {
+                m_lastETA = 0;
 
-				if(delta > 0.0001)
-					m_lastETA = estimateMAVG();
+                if(delta > 0.0001)
+                    m_lastETA = estimateMAVG();
 
-				if(m_lastETA == 0)
-					m_lastETA = estimateGASA();
-			}
+                if(m_lastETA == 0)
+                    m_lastETA = estimateGASA();
+            }
 
-			return m_lastETA;
-		}
+            return m_lastETA;
+        }
 
-		m_lastETA = estimateGASA();
+        m_lastETA = estimateGASA();
 
-		return m_lastETA;
-	}
+        return m_lastETA;
+    }
 
 }
 
--- src/torrent/timeestimator.h
+++ src/torrent/timeestimator.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2006 by Ivan Vasić   								   *
- *   ivasic@gmail.com   												   *
+ *   Copyright (C) 2006 by Ivan Vasić                                     *
+ *   ivasic@gmail.com                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -26,100 +26,93 @@
 
 namespace bt
 {
-	class TorrentControl;
+    class TorrentControl;
 
-	/**
-	 * Simple queue class for samples. Optimized for speed and size
-	 * without possibility to dynamically resize itself.
-	 * @author Ivan Vasic <ivasic@gmail.com>
-	 */
-	class SampleQueue
-	{
-	public:
-		SampleQueue(int max);
-		~SampleQueue();
+    /**
+     * Simple queue class for samples. Optimized for speed and size
+     * without possibility to dynamically resize itself.
+     * @author Ivan Vasic <ivasic@gmail.com>
+     */
+    class SampleQueue
+    {
+    public:
+        SampleQueue(int max);
+        ~SampleQueue();
 
-		/**
-		 * Inserts new sample into the queue. The oldest sample is overwritten.
-		 */
-		void push(Uint32 sample);
+        /**
+         * Inserts new sample into the queue. The oldest sample is overwritten.
+         */
+        void push(Uint32 sample);
 
-		Uint32 first();
-		Uint32 last();
+        Uint32 first();
+        Uint32 last();
 
-		bool isFull();
+        bool isFull();
 
-		/**
-		 * This function will return the number of samples in queue until it counts m_size number of elements.
-		 * After this point it will always return m_size since no samples are being deleted.
-		 */
-		int count();
+        /**
+         * This function will return the number of samples in queue until it counts m_size number of elements.
+         * After this point it will always return m_size since no samples are being deleted.
+         */
+        int count();
 
-		/**
-		 * Returns the sum of all samples.
-		 */
-		Uint32 sum();
+        /**
+         * Returns the sum of all samples.
+         */
+        Uint32 sum();
 
-	private:
-		int m_size;
-		int m_count;
+    private:
+        int m_size;
+        int m_count;
 
-		int m_start;
-		int m_end;
+        int m_start;
+        int m_end;
 
-		Uint32* m_samples;
-	};
+        Uint32* m_samples;
+    };
 
-	/**
-	 * ETA estimator class. It will use different algorithms for different download phases.
-	 * @author Ivan Vasic <ivasic@gmail.com>
-	*/
-	class KTORRENT_EXPORT TimeEstimator
-	{
-	public:
-		static const int NEVER = INT_MAX;
-		static const int ALREADY_FINISHED = 0;
+    /**
+     * ETA estimator class. It will use different algorithms for different download phases.
+     * @author Ivan Vasic <ivasic@gmail.com>
+    */
+    class KTORRENT_EXPORT TimeEstimator
+    {
+    public:
+        static const int NEVER = INT_MAX;
+        static const int ALREADY_FINISHED = 0;
 
-		enum ETAlgorithm
-		{
-			ETA_KT,		//ktorrent default algorithm - combination of the following according to our tests
-			ETA_CSA, 	//current speed algorithm
-			ETA_GASA,	//global average speed algorithm
-			ETA_WINX,	//window of X algorithm
-			ETA_MAVG	//moving average algorithm
-		};
+        enum ETAlgorithm
+        {
+            ETA_KT,     //ktorrent default algorithm - combination of the following according to our tests
+            ETA_CSA,    //current speed algorithm
+            ETA_GASA,   //global average speed algorithm
+            ETA_WINX,   //window of X algorithm
+            ETA_MAVG    //moving average algorithm
+        };
 
-		TimeEstimator(TorrentControl* tc);
-		~TimeEstimator();
+        TimeEstimator(TorrentControl* tc);
+        ~TimeEstimator();
 
-		///Returns ETA for m_tc torrent.
-		int estimate();
+        ///Returns ETA for m_tc torrent.
+        int estimate();
 
-		static void setAlgorithm(ETAlgorithm theValue);
-		static ETAlgorithm algorithm() { return m_algorithm; }
+    private:
+        int estimateGASA();
+        int estimateWINX();
+        int estimateMAVG();
+        int estimateKT();
 
-	private:
+        Uint32 sample() const;
+        Uint64 bytesLeft() const;
 
-		int estimateCSA();
-		int estimateGASA();
-		int estimateWINX();
-		int estimateMAVG();
-		int estimateKT();
+        TorrentControl* m_tc;
+        SampleQueue* m_samples;
 
-		Uint32 sample() const;
-		Uint64 bytesLeft() const;
+        Uint32 m_lastAvg;
+        int m_lastETA;
 
-		TorrentControl* m_tc;
-		SampleQueue* m_samples;
-
-		Uint32 m_lastAvg;
-		int m_lastETA;
-
-		//last percentage
-		double m_perc;
-
-		static ETAlgorithm m_algorithm;
-	};
+        //last percentage
+        double m_perc;
+    };
 
 }
 
