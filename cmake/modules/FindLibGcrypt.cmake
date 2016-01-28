b96538fb8c8c7f2529b7c3a69679c5479810f399
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2016-01-28 13:25:09 +0100

    Upgrade FindLibGcrypt.cmake
    it now provides:LibGcrypt_FOUND, LibGcrypt_VERSION, LibGcrypt_INCLUDE_DIRS, LibGcrypt_LIBRARIES and a new target: LibGcrypt::LibGcrypt

--- cmake/modules/FindLibGcrypt.cmake
+++ cmake/modules/FindLibGcrypt.cmake
@@ -1,91 +1,127 @@
-# - Try to find the libgcrypt library
-# Once done this will define
+#.rst:
+# FindLibGcrypt
+# -------------
 #
-#  LIBGCRYPT_FOUND - system has libgcrypt
-#  LIBGCRYPT_INCLUDE_DIR - the libgcrypt include directory
-#  LIBGCRYPT_LIBRARIES - Link these to use libgcrypt
-
-# Copyright (c) 2010 Joris Guisson <joris.guisson@gmail.com>
-# Copyright (c) 2007 Charles Connell <charles@connells.org> (This was based upon FindKopete.cmake)
+# Try to find libgcrypt.
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# This will define the following variables:
+#
+# ``LibGcrypt_FOUND``
+#     True if libgcrypt is available.
+#
+# ``LibGcrypt_VERSION``
+#     The version of LibGcrypt
+#
+# ``LibGcrypt_INCLUDE_DIRS``
+#     This should be passed to target_include_directories() if
+#     the target is not used for linking
+#
+# ``LibGcrypt_LIBRARIES``
+#     This can be passed to target_link_libraries() instead of
+#     the ``LibGcrypt::LibGcrypt`` target
+#
+# If ``LibGcrypt_FOUND`` is TRUE, the following imported target
+# will be available:
+#
+# ``LibGcrypt::LibGcrypt``
+#     The libgcrypt library
+#
+# Since 1.9.50.
 
-if(LIBGCRYPT_INCLUDE_DIR AND LIBGCRYPT_LIBRARIES)
-	
-# read from cache
-set(LIBGCRYPT_FOUND TRUE)
+#=============================================================================
+# Copyright 2007 Charles Connell <charles@connells.org> (This was based upon FindKopete.cmake)
+# Copyright 2010 Joris Guisson <joris.guisson@gmail.com>
+# Copyright 2016 Christophe Giboudeaux <cgiboudeaux@gmx.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
 
-else(LIBGCRYPT_INCLUDE_DIR AND LIBGCRYPT_LIBRARIES)
-	
-	FIND_PATH(LIBGCRYPT_INCLUDE_DIR 
-		NAMES
-		gcrypt.h
-		PATHS 
-		${KDE4_INCLUDE_DIR}
-		${INCLUDE_INSTALL_DIR}
-		PATH_SUFFIXES
-		libgcrypt
-	)
-	
-	FIND_LIBRARY(LIBGCRYPT_LIBRARIES 
-		NAMES
-		gcrypt
-		PATHS
-		${KDE4_LIB_DIR}
-		${LIB_INSTALL_DIR}
-	)
-	
-	if(LIBGCRYPT_INCLUDE_DIR AND LIBGCRYPT_LIBRARIES)
-		set(LIBGCRYPT_FOUND TRUE)
-	endif(LIBGCRYPT_INCLUDE_DIR AND LIBGCRYPT_LIBRARIES)
-		
-	if(MSVC)
-		FIND_LIBRARY(LIBGCRYPT_LIBRARIES_DEBUG 
-			NAMES
-			gcryptd
-			PATHS
-			${KDE4_LIB_DIR}
-			${LIB_INSTALL_DIR}
-		)
-		
-		if(NOT LIBGCRYPT_LIBRARIES_DEBUG)
-			set(LIBGCRYPT_FOUND FALSE)
-		endif(NOT LIBGCRYPT_LIBRARIES_DEBUG)
-				
-		if(MSVC_IDE)
-			if( NOT LIBGCRYPT_LIBRARIES_DEBUG OR NOT LIBGCRYPT_LIBRARIES)
-				message(FATAL_ERROR "\nCould NOT find the debug AND release version of the libgcrypt library.\nYou need to have both to use MSVC projects.\nPlease build and install both libgcrypt libraries first.\n")
-			endif( NOT LIBGCRYPT_LIBRARIES_DEBUG OR NOT LIBGCRYPT_LIBRARIES)
-		else(MSVC_IDE)
-			string(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_TOLOWER)
-			if(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
-				set(LIBGCRYPT_LIBRARIES ${LIBGCRYPT_LIBRARIES_DEBUG})
-			else(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
-				set(LIBGCRYPT_LIBRARIES ${LIBGCRYPT_LIBRARIES})
-			endif(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
-		endif(MSVC_IDE)
-	endif(MSVC)
-								
-	if(LIBGCRYPT_FOUND)
-		if(NOT LIBGCRYPT_FIND_QUIETLY)
-			message(STATUS "Found libgcrypt: ${LIBGCRYPT_LIBRARIES} ")
-		endif(NOT LIBGCRYPT_FIND_QUIETLY)
-	else(LIBGCRYPT_FOUND)
-		if(LIBGCRYPT_FIND_REQUIRED)
-			if(NOT LIBGCRYPT_INCLUDE_DIR)
-				message(FATAL_ERROR "Could not find libgcrypt includes.")
-			endif(NOT LIBGCRYPT_INCLUDE_DIR)
-			if(NOT LIBGCRYPT_LIBRARIES)
-				message(FATAL_ERROR "Could not find libgcrypt library.")
-			endif(NOT LIBGCRYPT_LIBRARIES)
-		else(LIBGCRYPT_FIND_REQUIRED)
-			if(NOT LIBGCRYPT_INCLUDE_DIR)
-				message(STATUS "Could not find libgcrypt includes.")
-			endif(NOT LIBGCRYPT_INCLUDE_DIR)
-			if(NOT LIBGCRYPT_LIBRARIES)
-				message(STATUS "Could not find libgcrypt library.")
-			endif(NOT LIBGCRYPT_LIBRARIES)
-		endif(LIBGCRYPT_FIND_REQUIRED)
-	endif(LIBGCRYPT_FOUND)																	
-endif(LIBGCRYPT_INCLUDE_DIR AND LIBGCRYPT_LIBRARIES)
+find_path(LibGcrypt_INCLUDE_DIRS
+    NAMES gcrypt.h
+    PATH_SUFFIXES libgcrypt
+)
+
+find_library(LibGcrypt_LIBRARIES
+    NAMES gcrypt
+)
+
+if(MSVC)
+    find_library(LibGcrypt_LIBRARIES_DEBUG
+        NAMES gcryptd
+    )
+
+    if(NOT LibGcrypt_LIBRARIES_DEBUG)
+        unset(LibGcrypt_LIBRARIES CACHE)
+    endif()
+
+    if(MSVC_IDE)
+        if(NOT (LibGcrypt_LIBRARIES_DEBUG AND LibGcrypt_LIBRARIES))
+            message(STATUS
+                "\nCould NOT find the debug AND release version of the libgcrypt library.\n
+                You need to have both to use MSVC projects.\n
+                Please build and install both libgcrypt libraries first.\n"
+            )
+            unset(LibGcrypt_LIBRARIES CACHE)
+        endif()
+    else()
+        string(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_TOLOWER)
+        if(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+            set(LibGcrypt_LIBRARIES ${LibGcrypt_LIBRARIES_DEBUG})
+        endif()
+    endif()
+endif()
+
+# Get version from gcrypt.h
+# #define GCRYPT_VERSION "1.6.4"
+if(LibGcrypt_INCLUDE_DIRS AND LibGcrypt_LIBRARIES)
+    file(STRINGS ${LibGcrypt_INCLUDE_DIRS}/gcrypt.h _GCRYPT_H REGEX "^#define GCRYPT_VERSION[ ]+.*$")
+    string(REGEX REPLACE "^.*GCRYPT_VERSION[ ]+\"([0-9]).([0-9]).([0-9])\".*$" "\\1" LibGcrypt_MAJOR_VERSION "${_GCRYPT_H}")
+    string(REGEX REPLACE "^.*GCRYPT_VERSION[ ]+\"([0-9]).([0-9]).([0-9])\".*$" "\\2" LibGcrypt_MINOR_VERSION "${_GCRYPT_H}")
+    string(REGEX REPLACE "^.*GCRYPT_VERSION[ ]+\"([0-9]).([0-9]).([0-9])\".*$" "\\3" LibGcrypt_PATCH_VERSION "${_GCRYPT_H}")
+
+    set(LibGcrypt_VERSION "${LibGcrypt_MAJOR_VERSION}.${LibGcrypt_MINOR_VERSION}.${LibGcrypt_PATCH_VERSION}")
+    unset(_GCRYPT_H)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibGcrypt
+    FOUND_VAR LibGcrypt_FOUND
+    REQUIRED_VARS LibGcrypt_INCLUDE_DIRS LibGcrypt_LIBRARIES
+    VERSION_VAR LibGcrypt_VERSION
+)
+
+if(LibGcrypt_FOUND AND NOT TARGET LibGcrypt::LibGcrypt)
+    add_library(LibGcrypt::LibGcrypt UNKNOWN IMPORTED)
+    set_target_properties(LibGcrypt::LibGcrypt PROPERTIES
+        IMPORTED_LOCATION "${LibGcrypt_LIBRARIES}"
+        INTERFACE_INCLUDE_DIRECTORIES "${LibGcrypt_INCLUDE_DIRS}")
+endif()
+
+mark_as_advanced(LibGcrypt_INCLUDE_DIRS LibGcrypt_LIBRARIES)
+
+include(FeatureSummary)
+set_package_properties(LibGcrypt PROPERTIES
+    URL "http://directory.fsf.org/wiki/Libgcrypt"
+    DESCRIPTION "General purpose crypto library based on the code used in GnuPG."
+)
