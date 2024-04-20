KDE/libktorrent

commit bb2d95837e0beaa6bdb978488ba378f8ee2e0c94

Add support for TR-064 IGD's

src/upnp/upnprouter.cpp
@@ -434,8 +434,6 @@ namespace bt
 		// add all the arguments for the command
 		QList<SOAP::Arg> args;
 		SOAP::Arg a;
-		a.element = "NewRemoteHost";
-		args.append(a);
 		
 		// the external port
 		a.element = "NewExternalPort";
@@ -495,8 +493,8 @@ namespace bt
 		// add all the arguments for the command
 		QList<SOAP::Arg> args;
 		SOAP::Arg a;
-		a.element = "NewRemoteHost";
-		args.append(a);
+		//a.element = "NewRemoteHost";
+		//args.append(a);
 		
 		// the external port
 		a.element = "NewExternalPort";

src/upnp/upnpmcastsocket.cpp
@@ -92,22 +92,34 @@ namespace bt
 	void UPnPMCastSocket::discover()
 	{
 		Out(SYS_PNP|LOG_NOTICE) << "Trying to find UPnP devices on the local network" << endl;
-		
+        
+        
 		// send a HTTP M-SEARCH message to 239.255.255.250:1900
-		const char* data = "M-SEARCH * HTTP/1.1\r\n" 
+		const char* upnp_data = "M-SEARCH * HTTP/1.1\r\n" 
 				"HOST: 239.255.255.250:1900\r\n"
 				"ST:urn:schemas-upnp-org:device:InternetGatewayDevice:1\r\n"
 				"MAN:\"ssdp:discover\"\r\n"
 				"MX:3\r\n"
 				"\r\n\0";
+				
+		const char* tr64_data = "M-SEARCH * HTTP/1.1\r\n" 
+                "HOST: 239.255.255.250:1900\r\n"
+                "ST:urn:dslforum-org:device:InternetGatewayDevice:1\r\n"
+                "MAN:\"ssdp:discover\"\r\n"
+                "MX:3\r\n"
+                "\r\n\0";
 		
 		if (d->verbose)
 		{
 			Out(SYS_PNP|LOG_NOTICE) << "Sending : " << endl;
-			Out(SYS_PNP|LOG_NOTICE) << data << endl;
+            Out(SYS_PNP|LOG_NOTICE) << upnp_data << endl;
+            
+            Out(SYS_PNP|LOG_NOTICE) << "Sending : " << endl;
+            Out(SYS_PNP|LOG_NOTICE) << tr64_data << endl;
 		}
 		
-		writeDatagram(data,strlen(data),QHostAddress("239.255.255.250"),1900);
+		writeDatagram(upnp_data,strlen(upnp_data),QHostAddress("239.255.255.250"),1900);
+        writeDatagram(tr64_data,strlen(tr64_data),QHostAddress("239.255.255.250"),1900);
 	}
 	
 	void UPnPMCastSocket::onXmlFileDownloaded(UPnPRouter* r,bool success)

commit d964823e8eb51386090a8bed5a282c8ae5145698

upnprouter: Send <NewRemoteHost> in forward and undoForward
NewRemoteHost is a required argument for both AddPortMapping
and DeletePortMapping in both UPnP WANIPConnection:1 and TR-064.
To specify a wildcard value, an empty string is used.

Omitting the tag makes some gateways respond with 402 Invalid Args
faultcode.

Fixes: bb2d95837e0b ("Add support for TR-064 IGD's")

src/upnp/upnprouter.cpp
@@ -387,6 +387,8 @@ void UPnPRouter::UPnPRouterPrivate::forward(const UPnPService *srv, const net::P
     // add all the arguments for the command
     QList<SOAP::Arg> args;
     SOAP::Arg a;
+    a.element = "NewRemoteHost";
+    args.append(a);
 
     // the external port
     a.element = "NewExternalPort";
@@ -445,8 +447,8 @@ void UPnPRouter::UPnPRouterPrivate::undoForward(const UPnPService *srv, const ne
     // add all the arguments for the command
     QList<SOAP::Arg> args;
     SOAP::Arg a;
-    // a.element = "NewRemoteHost";
-    // args.append(a);
+    a.element = "NewRemoteHost";
+    args.append(a);
 
     // the external port
     a.element = "NewExternalPort";