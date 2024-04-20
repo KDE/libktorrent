KDE/libktorrent

commit d671f62febfe2bedeae9c427c58a02675dabd9cb

Use QNetworkAccessManager to send and receive UPnP requests.
Port forwarding and unforwarding successfully works here, however
GetExternalIP command still fails for some reason.

src/upnp/upnprouter.cpp
@@ -17,9 +17,14 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  ***************************************************************************/
-#include <stdlib.h>
+
+#include <cstdlib>
+
 #include <QDir>
+#include <QDomElement>
+#include <QNetworkRequest>
 #include <QStringList>
+
 #include <klocalizedstring.h>
 #include <kio/job.h>
 #include <torrent/globals.h>
@@ -35,7 +40,6 @@
 #include "upnpdescriptionparser.h"
 #include "soap.h"
 #include "httprequest.h"
-#include <QDomElement>
 
 using namespace net;
 
@@ -150,7 +154,7 @@ namespace bt
 
     void UPnPRouter::addService(const UPnPService& s)
     {
-        foreach(const UPnPService& os, d->services)
+        for (const UPnPService& os : qAsConst(d->services))
         {
             if(s.servicetype == os.servicetype)
                 return;
@@ -200,7 +204,7 @@ namespace bt
         bool found = false;
         Out(SYS_PNP | LOG_NOTICE) << "Forwarding port " << port.number << " (" << (port.proto == UDP ? "UDP" : "TCP") << ")" << endl;
         // first find the right service
-        foreach(const UPnPService& s, d->services)
+        for (const UPnPService& s : qAsConst(d->services))
         {
             if(s.servicetype.contains("WANIPConnection") || s.servicetype.contains("WANPPPConnection"))
             {
@@ -370,7 +374,7 @@ namespace bt
 
     void UPnPRouter::visit(UPnPRouter::Visitor* visitor) const
     {
-        foreach(const Forwarding& fwd, d->fwds)
+        for (const Forwarding& fwd : qAsConst(d->fwds))
         {
             visitor->forwarding(fwd.port, fwd.pending_req != 0, fwd.service);
         }
@@ -386,9 +390,8 @@ namespace bt
 
     UPnPRouter::UPnPRouterPrivate::~UPnPRouterPrivate()
     {
-        foreach(HTTPRequest* r, active_reqs)
+        for (HTTPRequest* r : qAsConst(active_reqs))
         {
-            r->cancel();
             r->deleteLater();
         }
     }
@@ -405,27 +408,19 @@ namespace bt
         QString host = !ctrlurl.host().isEmpty() ? ctrlurl.host() : location.host();
         bt::Uint16 port = ctrlurl.port() != -1 ? ctrlurl.port() : location.port(80);
 
-        QString http_hdr;
-        QTextStream out(&http_hdr);
-        QString encoded_query = ctrlurl.query();
-        if(encoded_query.isEmpty())
-            out << "POST " << ctrlurl.path() << " HTTP/1.1\r\n";
-        else
-            out << "POST " << ctrlurl.path() << "?" << encoded_query << " HTTP/1.1\r\n";
-        out << "Host: " << host << ":" << port << "\r\n";
-        out << "User-Agent: " << bt::GetVersionString() << "\r\n";
-        out << "Content-length: $CONTENT_LENGTH\r\n";
-        out << "Content-Type: text/xml\r\n";
-        out << "SOAPAction: \"" << soapact << "\"\r\n\r\n";
+        QNetworkRequest networkReq;
+        networkReq.setUrl(ctrlurl);
+        networkReq.setRawHeader("Host", host.toLatin1() + QByteArrayLiteral(":") + QByteArray::number(port));
+        networkReq.setRawHeader("User-Agent", bt::GetVersionString().toLatin1());
+        networkReq.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/xml"));
+        networkReq.setRawHeader("SOAPAction", soapact.toLatin1());
 
-        HTTPRequest* r = new HTTPRequest(http_hdr, query, host, port, verbose);
+        HTTPRequest* r = new HTTPRequest(networkReq, query, host, port, verbose);
         if(!at_exit)
         {
             // Only listen for results when we are not exiting
             active_reqs.append(r);
         }
-
-        r->start();
         return r;
     }
 
@@ -519,7 +514,7 @@ namespace bt
 
     void UPnPRouter::UPnPRouterPrivate::getExternalIP()
     {
-        foreach(const UPnPService& s, services)
+        for (const UPnPService& s : qAsConst(services))
         {
             if(s.servicetype.contains("WANIPConnection") || s.servicetype.contains("WANPPPConnection"))
             {

src/upnp/httprequest.cpp
@@ -15,146 +15,68 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  ***************************************************************************/
-#include <QTimer>
+
 #include <QHostAddress>
+#include <QNetworkReply>
 #include <QStringList>
-#include <klocalizedstring.h>
+#include <QTimer>
+
+#include <KLocalizedString>
+
 #include <util/log.h>
 #include "httprequest.h"
-
-
-
 
 namespace bt
 {
 
-	HTTPRequest::HTTPRequest(const QString & hdr,const QString & payload,const QString & host,Uint16 port,bool verbose) 
-		: hdr(hdr),payload(payload),verbose(verbose),host(host),port(port),finished(false),success(false)
+	HTTPRequest::HTTPRequest(const QNetworkRequest & hdr,const QString & payload,const QString & host,Uint16 port,bool verbose) 
+		: hdr(hdr),m_payload(payload),verbose(verbose),host(host),port(port),success(false)
 	{
-		sock = new QTcpSocket(this);
-		connect(sock,SIGNAL(readyRead()),this,SLOT(onReadyRead()));
-		connect(sock,SIGNAL(error(QAbstractSocket::SocketError)),this,SLOT(onError(QAbstractSocket::SocketError)));
-		connect(sock,SIGNAL(connected()),this, SLOT(onConnect()));
+		networkAccessManager = new QNetworkAccessManager(this);
+		connect(networkAccessManager, &QNetworkAccessManager::finished, this, &HTTPRequest::replyFinished);
+		networkAccessManager->connectToHost(host,port);
+
+		QTcpSocket socket;
+		QString localAddress;
+		socket.connectToHost(host, port);
+		if (socket.waitForConnected()) {
+			localAddress = socket.localAddress().toString();
+                        socket.close();
+		}
+		else {
+			Out(SYS_PNP|LOG_DEBUG) << "TCP connection timeout" << endl;
+			socket.close();
+			error = i18n("Operation timed out");
+                        success = false;
+			emit result(this);
+			operationFinished(this);
+			return;
+		}
+
+		m_payload = m_payload.replace(QLatin1String("$LOCAL_IP"), localAddress);
 	}
-	
-	
-	HTTPRequest::~HTTPRequest()
-	{
-		sock->close();
-	}
-	
+
 	void HTTPRequest::start()
 	{
-		success = false;
-		finished = false;
-		reply.clear();
-		sock->connectToHost(host,port);
-		QTimer::singleShot(30000,this,SLOT(onTimeout()));
+		networkAccessManager->post(hdr, m_payload.toLatin1());
 	}
-	
-	void HTTPRequest::cancel()
-	{
-		finished = true;
-		sock->close();
-	}
-	
-	void HTTPRequest::onConnect()
-	{
-		if (finished)
-			return;
-		
-		payload = payload.replace(QLatin1String("$LOCAL_IP"),sock->localAddress().toString());
-		hdr = hdr.replace(QLatin1String("$CONTENT_LENGTH"),QString::number(payload.length()));
-			
-		QString req = hdr + payload;
-		if (verbose)
-		{
-			Out(SYS_PNP|LOG_DEBUG) << "Sending " << endl;
-			QStringList lines = hdr.split(QLatin1String("\r\n"));
-			foreach (const QString &line,lines)
-				Out(SYS_PNP|LOG_DEBUG) << line << endl;
-			
-			Out(SYS_PNP|LOG_DEBUG) << payload << endl;
-		}
 
-		sock->write(req.toLatin1());
-	}
-	
-	void HTTPRequest::onReadyRead()
+	void HTTPRequest::replyFinished(QNetworkReply *networkReply)
 	{
-		if (finished)
-			return;
-		
-		Uint32 ba = sock->bytesAvailable();
-		if (ba == 0)
+		if(networkReply->error())
 		{
-			if (!finished)
-			{
-				error = i18n("Connection closed unexpectedly");
-				success = false;
-				finished = true;
-				result(this);
-				operationFinished(this);
-			}
-			sock->close();
+			error = networkReply->errorString();
+			success = false;
+			emit result(this);
+			operationFinished(this);
 			return;
 		}
-		
-		reply.append(sock->read(ba));
-		int eoh = reply.indexOf("\r\n\r\n");
-		if (eoh != -1)
-		{
-			/*reply_header = QHttpResponseHeader(QString::fromAscii(reply.mid(0,eoh + 4))); PORT: KF5
-			if (reply_header.contentLength() > 0 && reply.size() < eoh + 4 + reply_header.contentLength())
-			{
-				// Haven't got full content yet, so return and wait for more
-				return;
-			}
-			else
-			{
-				reply = reply.mid(eoh + 4);
-				success = reply_header.statusCode() == 200;
-				if (!success)
-					error = reply_header.reasonPhrase();
-				finished = true;
-				result(this);
-				operationFinished(this);
-			}*/
-		}
-		else
-			return;
-	}
-	
-	void HTTPRequest::onError(QAbstractSocket::SocketError err)
-	{
-		Q_UNUSED(err);
-		if (finished)
-			return;
-		
-		Out(SYS_PNP|LOG_DEBUG) << "HTTPRequest error : " << sock->errorString() << endl;
-		success = false;
-		finished = true;
-		sock->close();
-		error = sock->errorString();
-		result(this);
+		reply = networkReply->readAll();
+		success = true;
+		emit result(this);
 		operationFinished(this);
-	}
-	
-	void HTTPRequest::onTimeout()
-	{
-		if (finished)
-			return;
-
-		Out(SYS_PNP|LOG_DEBUG) << "HTTPRequest timeout" << endl;
-		success = false;
-		finished = true;
-		sock->close();
-		error = i18n("Operation timed out");
-		result(this);
-		operationFinished(this);
-	}
-
+}
 
 }

src/upnp/httprequest.h
@@ -17,10 +17,13 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  ***************************************************************************/
+
 #ifndef KTHTTPREQUEST_H
 #define KTHTTPREQUEST_H
 
-#include <QTcpSocket>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+
 #include <interfaces/exitoperation.h>
 #include <util/constants.h>
 
@@ -46,19 +49,13 @@ namespace bt
 		 * @param port THe port
 		 * @param verbose Print traffic to the log
 		 */
-		HTTPRequest(const QString & hdr,const QString & payload,const QString & host,
+		HTTPRequest(const QNetworkRequest & hdr,const QString & payload,const QString & host,
 					bt::Uint16 port,bool verbose);
-		virtual ~HTTPRequest();
-		
 		/**
-		 * Open a connetion and send the request.
+		 * Open a connection and send the request.
 		 */
 		void start();
-		
-		/**
-			Cancel the request, no result signal will be emitted.
-		*/
-		void cancel();
+
 		
 		/**
 			Get the reply data
@@ -75,37 +72,31 @@ namespace bt
 		*/
 		QString errorString() const {return error;}
 		
-	signals:
+	Q_SIGNALS:
 		/**
 		 * An OK reply was sent.
 		 * @param r The sender of the request
-		 * @param data The data of the reply
 		 */
 		void result(HTTPRequest* r);
 		
-		
-	private slots:
-		void onReadyRead();
-		void onError(QAbstractSocket::SocketError err);
-		void onTimeout();
-		void onConnect();
-	
+
+	public:
+		void replyFinished(QNetworkReply *networkReply);
+
 	private:
 		void parseReply(int eoh);
-		
+
 	private:
-		QTcpSocket* sock;
-		QString hdr,payload;
+		QNetworkRequest hdr;
+		QString m_payload;
 		bool verbose;
 		QString host;
 		bt::Uint16 port;
-		bool finished;
-// 		QHttpResponseHeader reply_header; PORT: KF5
+                QNetworkAccessManager *networkAccessManager;
 		QByteArray reply;
 		bool success;
 		QString error;
 	};
-
 }
 
 #endif

src/upnp/soap.cpp
@@ -17,6 +17,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  ***************************************************************************/
+
 #include "soap.h"
 
 namespace bt

src/upnp/soap.h
@@ -17,6 +17,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  ***************************************************************************/
+
 #ifndef KTSOAP_H
 #define KTSOAP_H
 

src/upnp/upnpdescriptionparser.cpp
@@ -17,8 +17,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  ***************************************************************************/
-#include <qxml.h>
-#include <qstack.h>
+
+#include <QtXml>
+#include <QStack>
+
 #include <util/fileops.h>
 #include <util/log.h>
 #include "upnprouter.h"

src/upnp/upnpmcastsocket.cpp
@@ -18,7 +18,11 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  ***************************************************************************/
 
+#include <QFile>
+#include <QStringList>
+#include <QTextStream>
 #include <QUrl>
+
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -27,9 +31,6 @@
 #include <netinet/ip.h>
 #endif
 #include <arpa/inet.h>
-#include <QStringList>
-#include <QFile>
-#include <QTextStream>
 #include <util/log.h>
 #include "upnpmcastsocket.h"
 

src/upnp/upnpmcastsocket.h
@@ -17,6 +17,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.             *
  ***************************************************************************/
+
 #ifndef KTUPNPMCASTSOCKET_H
 #define KTUPNPMCASTSOCKET_H
 

commit bf5de253b08183d040ea45aec006464aec93918d

upnprouter: Fix SOAPAction header in UPnP requests
The header's value must be enclosed by quotation marks:
https://www.w3.org/TR/2000/NOTE-SOAP-20000508/#_Toc478383528

Some gateways respond with 400 Bad Request when this requirement
isn't met.

Fixes: d671f62febfe ("Use QNetworkAccessManager to send and receive UPnP requests.")

src/upnp/upnprouter.cpp
@@ -371,7 +371,7 @@ HTTPRequest *UPnPRouter::UPnPRouterPrivate::sendSoapQuery(const QString &query, 
     networkReq.setRawHeader("Host", host.toLatin1() + QByteArrayLiteral(":") + QByteArray::number(port));
     networkReq.setRawHeader("User-Agent", bt::GetVersionString().toLatin1());
     networkReq.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/xml"));
-    networkReq.setRawHeader("SOAPAction", soapact.toLatin1());
+    networkReq.setRawHeader("SOAPAction", QByteArrayLiteral("\"") + soapact.toLatin1() + QByteArrayLiteral("\""));
 
     HTTPRequest *r = new HTTPRequest(networkReq, query, host, port, verbose);
     if (!at_exit) {